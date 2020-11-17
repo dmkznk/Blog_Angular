ce38b5aa40a8a8ac092c2ea70b081850f7381150
Dima Kuzenko <dimas@Dimas-MacBook-Pro.local>
2020-11-17 21:30:01 +0200

    first commit

--- src/app/admin/admin-module.ts
+++ src/app/admin/admin-module.ts
@@ -1,18 +1,32 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import { AdminLayuotComponent } from './shared/components/admin-layuot/admin-layuot.component';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import {CreatePageComponent} from './create-page/create-page.component';
+import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {EditPageComponent} from './edit-page/edit-page.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [AdminLayuotComponent, LoginPageComponent],
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
+  ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
-      {path: '', component: AdminLayuotComponent, children: [
+      {path: '', component: AdminLayoutComponent, children: [
           {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
-          {path: 'login', component: LoginPageComponent}
-
+          {path: 'login', component: LoginPageComponent},
+          {path: 'create', component: CreatePageComponent},
+          {path: 'dashboard', component: DashboardPageComponent},
+          {path: 'post/:id/edit', component: EditPageComponent}
         ]}
     ])
   ],
